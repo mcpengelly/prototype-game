067c416e173f81d70c0f30c5cd0300d60c9999ce
Malaz Alamir <malaz.alamir@hotmail.com>
2015-09-06 16:16:43 -0400

    Built Rough Scene

---
+++ 2d-prototype-game/Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public float ballInitialVelocity = 600;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Awake () {
+		rb = GetComponent<Rigidbody> ();
+		rb.AddForce (new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ 2d-prototype-game/Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddleSpeed = 1;
+	private Vector3 playerPos = new Vector3(0, -9.5f, 0);
+
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+		
+		if (Input.touchCount == 1) {
+			Touch touch = Input.GetTouch(0);
+			
+			if (touch.position.x > Screen.width/2)
+			{
+				xPos = transform.position.x +  paddleSpeed;
+			}else{
+				xPos = transform.position.x - paddleSpeed;
+			}
+			//xPos = -7.5f + 15 * touch.position.x/Screen.width;
+			
+		}
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -8f, 8f), -9.5f, 0f);
+		transform.position = playerPos;
+	}
+}
