375f4ca92ed32ae0f0385173b2e10c6bd5e8be2f
Malaz Alamir <malaz.alamir@hotmail.com>
2015-09-11 23:50:24 -0400

    Added CPU AI script, added random direction for PongBall

--- 2d-prototype-game/Assets/Scripts/PongAI.cs
+++ 2d-prototype-game/Assets/Scripts/PongAI.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class PongAI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+		Transform Enemy; 
+		//public float speed = 1.0f;
+		 
+	// you will find that the paddle repositions its center to where the ball is, causing the paddle to slightly pass top and bottom walls.
+	// Error to fix: the ball sometimes passes through the top and bottom walls when colliding with paddles on sharp angle (CPU also moves out of frame)
+		void FixedUpdate ()
+		{
+		Enemy = GameObject.FindWithTag ("ball").transform;
+			transform.position = new Vector2 
+				(transform.position.x, Enemy.position.y);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
--- 2d-prototype-game/Assets/Scripts/PongBall.cs
+++ 2d-prototype-game/Assets/Scripts/PongBall.cs
@@ -2,17 +2,40 @@
 using System.Collections;
 
 public class PongBall : MonoBehaviour {
-
-	public float ballInitialVelocity = 600;
 	private Rigidbody2D rb;
-	// Use this for initialization
-	void Awake () {
-		rb = GetComponent<Rigidbody2D> ();
-		rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	public float ballInitialVelocity = 600;
 
+	// Starts ball moving. edited so that the ball starts off with an initial velocity but to a random direction - towards player vs towards cpu
+	void Start() {
+		rb = GetComponent<Rigidbody2D> ();
+		float random = Random.Range (0.0f, 100.0f);
+		if (random < 50.0f) {
+			rb.AddForce (new Vector2 (ballInitialVelocity, ballInitialVelocity));
+		} else {
+			rb.AddForce(new Vector2(-ballInitialVelocity, -ballInitialVelocity));
+		}
 	}
 }
+	// trying to make a method that changes the velocity of the ball depending which angle it collides with paddles... seems ball slows down otherwise?
+	//void onCollisionEnter2D(Collision2D deflect) {
+	//	if (deflect.collider.CompareTag ("Player") || deflect.collider.CompareTag ("CPU")) {
+	//		var speedY = Rigidbody2D.velocity;
+	//		speedY.y = (speedY.y/2.0f));
+	//		Rigidbody2D.velocity = speedY;
+	//	}
+	//}
+
+// Your previous code segment
+//  public float ballInitialVelocity = 600;
+//private Rigidbody2D rb;
+// Use this for initialization
+//void Awake () {
+//  rb = GetComponent<Rigidbody2D> ();
+// rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+//}
+
+// Update is called once per frame
+//void Update () {
+
+//}
+
